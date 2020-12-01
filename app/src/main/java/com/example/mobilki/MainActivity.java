5088a6e7e1eeaa9f6ab3db4e7717112dbe2ecf55
Patryk Kaca <patrykaca@gmail.com>
2020-12-01 16:18:08 +0100

    login screen

--- app/src/main/java/com/example/mobilki/MainActivity.java
+++ app/src/main/java/com/example/mobilki/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.mobilki;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
+        try {  //hide title bar in login screen
+            Objects.requireNonNull(this.getSupportActionBar()).hide();
+        }
+        catch (NullPointerException ignored) {
+
+        }
+        Button loginBtn = (Button)findViewById(R.id.loginBtn);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //exit login activity and starts app
+                Intent startIntend = new Intent(getApplicationContext(), HomeActivity.class);
+                startActivity(startIntend);
+            }
+        });
     }
 }
