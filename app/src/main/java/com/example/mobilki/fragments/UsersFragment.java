db5f3fa1f79dc8fa4ba9e43e74ab0696aa806175
Kondziii <1999konrad@gmail.com>
2021-01-05 20:16:07 +0100

    part 13 done

--- app/src/main/java/com/example/mobilki/fragments/UsersFragment.java
+++ app/src/main/java/com/example/mobilki/fragments/UsersFragment.java
@@ -7,9 +7,12 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.example.mobilki.R;
 import com.example.mobilki.User;
@@ -20,11 +23,13 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.Queue;
 
 public class UsersFragment extends Fragment {
 
@@ -35,6 +40,7 @@ public class UsersFragment extends Fragment {
     private RecyclerView recyclerView;
     private UserAdapter userAdapter;
     private List<User> userList;
+    private EditText search_users;
 
 
     public static UsersFragment newInstance(String param1, int param2) {
@@ -63,10 +69,91 @@ public class UsersFragment extends Fragment {
         recyclerView = view.findViewById(R.id.users_recycler_view);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-
         userList = new ArrayList<>();
         readUsers();
+        search_users = view.findViewById(R.id.search_users);
+        search_users.setText("Jan Pa");
+        search_users.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                filterUsers(s.toString().toLowerCase());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
         return view;
+    }
+
+    private void filterUsers(final String s) {
+        final FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+//        Query query = FirebaseDatabase.getInstance().getReference("Users").orderByChild("firstname").startAt(s).endAt(s + "\uf8ff");
+//        query.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                userList.clear();
+//                for(DataSnapshot dataSnapshot : snapshot.getChildren()) {
+//                    User _user = dataSnapshot.getValue(User.class);
+//
+//                    if(!Objects.equals(firebaseUser.getUid(), _user.getId())) {
+//                        userList.add(_user);
+//                    }
+//                }
+//                userAdapter = new UserAdapter(getContext(), userList, false);
+//                recyclerView.setAdapter(userAdapter);
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError error) {
+//
+//            }
+//        });
+        final DatabaseReference firebaseDatabase = FirebaseDatabase.getInstance().getReference("Users");
+        firebaseDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                userList.clear();
+                for(DataSnapshot snapshot1: snapshot.getChildren()) {
+                    User _user = snapshot1.getValue(User.class);
+                    if(!Objects.equals(_user.getId(), firebaseUser.getUid())) {
+                        if(!s.contains(" ")) {
+                            if(_user.getFirstName().toLowerCase().contains(s) || _user.getLastName().toLowerCase().contains(s)) {
+                                userList.add(_user);
+                            }
+                        }
+                        else {
+                            String [] parts = s.split(" ");
+                            try {
+                                if ((_user.getFirstName().toLowerCase().contains(parts[0]) && _user.getLastName().toLowerCase().contains(parts[1]))
+                                || (_user.getFirstName().toLowerCase().contains(parts[1]) && _user.getLastName().toLowerCase().contains(parts[0]))) {
+                                    userList.add(_user);
+                                }
+                            }catch (ArrayIndexOutOfBoundsException e) {
+                                if(_user.getFirstName().toLowerCase().contains(parts[0])
+                                        || _user.getLastName().toLowerCase().contains(parts[0])) {
+                                    userList.add(_user);
+                                }
+                            }
+                        }
+                    }
+                }
+                userAdapter = new UserAdapter(getContext(), userList, false);
+                recyclerView.setAdapter(userAdapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     private void readUsers() {
@@ -75,20 +162,24 @@ public class UsersFragment extends Fragment {
         databaseReference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                userList.clear();
-                for(DataSnapshot snapshot1: snapshot.getChildren()) {
-                    User user = snapshot1.getValue(User.class);
+
+                if(Objects.equals(search_users.getText().toString(), "")) {
+                    userList.clear();
+                    for(DataSnapshot snapshot1: snapshot.getChildren()) {
+                        User user = snapshot1.getValue(User.class);
 
 
-                    assert firebaseUser != null;
-                    assert user != null;
-                    if(!Objects.equals(user.getId(),firebaseUser.getUid())) {
-                        userList.add(user);
+                        assert firebaseUser != null;
+                        assert user != null;
+                        if(!Objects.equals(user.getId(),firebaseUser.getUid())) {
+                            userList.add(user);
+                        }
                     }
+
+                    userAdapter = new UserAdapter(getContext(), userList, false);
+                    recyclerView.setAdapter(userAdapter);
                 }
 
-                userAdapter = new UserAdapter(getContext(), userList, false);
-                recyclerView.setAdapter(userAdapter);
 
             }
 
